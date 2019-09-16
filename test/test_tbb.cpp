4d0820dff9d228a190cc3c63371bab6eeb1eff21
JeongJu Kim <haje01@gmail.com>
2019-09-16 23:28:40 +0900

    test tbb

--- test/test_tbb.cpp
+++ test/test_tbb.cpp
@@ -1,8 +1,28 @@
-#include <tbb/parallel_for.h>
+#include <iostream>
 #include <catch2/catch.hpp>
+#include <tbb/tbb.h>
+
+using namespace tbb;
+
+float power( float a) {
+    return a * a;
+}
+
+class ApplyPower {
+    float * const my_a;
+public:
+    void operator()( const blocked_range<size_t> & r) const {
+        float * a = my_a;
+        for(size_t i= r.begin(); i != r.end(); ++i) 
+            power(a[i]);
+    }
+    ApplyPower( float a[] ) : my_a(a) {}
+};
+
 
 TEST_CASE("TBB test.", "Test1") {
     SECTION("sa") {
-       ;
+        float a[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0};
+        parallel_for(blocked_range<size_t>(0, 5), ApplyPower(a));
     }
 }
